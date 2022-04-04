4c06f1387c82f88a5e188320a4e412a0f07c66c4
I0HuKc <web-disain@mail.ua>
2022-04-04 18:43:49 +0300

    убрал тест валидации, т. к. перенес ее на шлюз

--- internal/server/user_handlers_test.go
+++ internal/server/user_handlers_test.go
@@ -18,66 +18,14 @@ func Test_Server_UserHandlers_CreateUser(t *testing.T) {
 	defer conn.Close()
 
 	client := pb.NewUserServiceClient(conn)
-	testCases := []struct {
-		name    string
-		payload pb.NewUserReq
-		isValid bool
-	}{
-		{
-			name: "empty_login",
-			payload: pb.NewUserReq{
-				Email:    "kiwi@gmail.com",
-				Password: "somepasswordhere1",
-			},
-			isValid: false,
-		},
-		{
-			name: "empty_email",
-			payload: pb.NewUserReq{
-				Login:    "I0HuKc",
-				Password: "somepasswordhere2",
-			},
-			isValid: false,
-		},
-		{
-			name: "empty_password",
-			payload: pb.NewUserReq{
-				Login: "I0HuKc",
-				Email: "kiwi@gmail.com",
-			},
-			isValid: true,
-		},
-		{
-			name: "invalid_email",
-			payload: pb.NewUserReq{
-				Login:    "I0HuKc",
-				Email:    "invalid",
-				Password: "somepasswordhere4",
-			},
-			isValid: false,
-		},
-		{
-			name: "valid",
-			payload: pb.NewUserReq{
-				Login:    "I0HuKc",
-				Email:    "kiwi@gmail.com",
-				Password: "somepasswordhere5",
-			},
-			isValid: true,
-		},
-	}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			_, err := client.CreateUser(ctx, &tc.payload)
-
-			if tc.isValid {
-				assert.NoError(t, err)
-				return
-			}
-
-			assert.Error(t, err)
+	t.Run("create_user", func(t *testing.T) {
+		_, err := client.CreateUser(ctx, &pb.NewUserReq{
+			Login:    "I0HuKc",
+			Email:    "kiwi@gmail.com",
+			Password: "somepasswordhere5",
 		})
-	}
 
+		assert.NoError(t, err)
+	})
 }
